fe0781ff7e00219a861f32e01435320e7ca0e275
Nate Harris <nathanharris.sf@gmail.com>
2018-09-11 16:03:05 -0700

    Write delete methods and update readme

--- README.md
+++ README.md
@@ -29,4 +29,53 @@ class BinarySearchTree
 end
 ```
 
-Our first node into the tree becomes our root.
+Our first node into the tree becomes our root. We'll need a way to handle our node insertion. `insert(value)` will start the process.
+
+```ruby
+# Assign our root to the return value of insert_node(tree_node, value)
+def insert(value)
+  @root = insert_node(@root, value)
+end
+
+private
+
+def insert_node(tree_node, value)
+
+  # return our new node if we find an empty spot
+  return Node.new(value) unless tree_node
+
+  if value <= tree_node.value
+    # Our value is less than or equal to the current node
+    # so we'll recurse on the node's left
+    tree_node.left = insert_node(tree_node.left, value)
+  elsif value > tree_node.value
+    # Our value is greater than the current node
+    # so we'll recurse on the node's right
+    tree_node.right = insert_node(tree_node.right, value)
+  end
+
+  # return the current node being evaluated
+  tree_node
+end
+```
+
+Peachy. But let's say we want to find a node? We'll start with our target value and compare a starting node. If the value is less than node, we'll move on to the node's left. If the value is greater, than we'll move onto the node's right. If our value is equal to the node, bingo - we've found it. If we attempt to move to a nil node, we'll know our value is not in the tree.
+
+```ruby
+def find(value, tree_node = @root)
+  # Node does not exist
+  return nil unless tree_node
+
+  case value <=> tree_node.value
+  when -1
+    # Value is less than node, recurse on left
+    find(value, tree_node.left)
+  when 0
+    # Found it!
+    tree_node
+  when 1
+    # Value is greater than node, recurse on right
+    find(value, tree_node.right)
+  end
+end
+```
--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -11,6 +11,29 @@ class BinarySearchTree
     @root = insert_node(@root, value)
   end
 
+  def delete(value)
+    @root = delete_node(@root, value)
+  end
+
+  def find(value, tree_node = @root)
+    return nil unless tree_node
+    case value <=> tree_node.value
+    when -1
+      find(value, tree_node.left)
+    when 0
+      tree_node
+    when 1
+      find(value, tree_node.right)
+    end
+  end
+
+  def in_order(node = @root)
+    return [] unless node
+    in_order(node.left) + [node.value] + in_order(node.right)
+  end
+
+  private
+
   def insert_node(tree_node, value)
     return Node.new(value) unless tree_node
     if value <= tree_node.value
@@ -21,8 +44,33 @@ class BinarySearchTree
     tree_node
   end
 
-  def in_order(tree_node = @root)
-    return [] unless tree_node
-    in_order(tree_node.left) + [tree_node.value] + in_order(tree_node.right)
+  def delete_node(tree_node, value)
+    case value <=> tree_node.value
+    when -1
+      tree_node.left = delete_step(tree_node.left, value)
+    when 0
+      tree_node = remove_node(tree_node)
+    when 1
+      tree_node.right = delete_step(tree_node.right, value)
+    end
+    tree_node
+  end
+
+  def remove_node(tree_node)
+    if tree_node.has_children?
+      return tree_node.left unless tree_node.right
+      return tree_node.right unless tree_node.left
+      replacement = maximum(tree_node.left)
+
+      if replacement.left && tree_node.left.right
+        parent, child = tree_node.left, tree_node.left.right
+        parent, child = parent.right, child.right while child.right
+        parent.right = child.left
+      end
+
+      replacement.left = tree_node.left
+      replacement.right = tree_node.right
+      replacement
+    end
   end
 end
